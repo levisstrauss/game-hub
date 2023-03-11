9278fc77a28cc4a57cf22f9682bb2a0b7a26896f
levisstrauss <levisstrauss11@yahoo.com>
2023-03-11 17:05:05 -0500

    Highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,8 +5,9 @@ import {getCroppedImageUrl} from "../services/image-url";
 
 interface Props {
     onSelectedGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
-export const GenreList = ({onSelectedGenre}: Props) => {
+export const GenreList = ({selectedGenre, onSelectedGenre}: Props) => {
 
     const { data, isLoading, error } = useGenres();
     if (error) return null;
@@ -22,7 +23,7 @@ export const GenreList = ({onSelectedGenre}: Props) => {
                         borderRadius={8}
                         src={getCroppedImageUrl(genre.image_background)}
                     />
-                    <Button onClick={() => onSelectedGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+                    <Button fontWeight={genre.id === selectedGenre?.id ? 'bold': 'normal'} onClick={() => onSelectedGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
                     </HStack>
                 </ListItem>
             ))}
